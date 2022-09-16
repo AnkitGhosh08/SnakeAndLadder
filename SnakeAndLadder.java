f46f797c81f5235ba7958a8349e7ec8e584a1ced
Ankit Ghosh <ankitghosh877@gmail.com>
2022-09-16 15:32:25 +0530

    welcome

---
+++ SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.codinclub;
+
+public class SnakeAndLadder
+{
+        public static void main(String[] args)
+        {
+            System.out.println("Welcome to Snake And ladder problem");
+        }
+}
+
