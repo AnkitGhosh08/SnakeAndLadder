4aaf432f8a9f570a2e772416a53fbf2d610550da
Ankit Ghosh <ankitghosh877@gmail.com>
2022-09-16 16:44:22 +0530

    Rolling dice

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,11 +1,25 @@
 package com.codinclub;
 
-public class SnakeAndLadder
-{
-        public static void main(String[] args)
-        {
-            int playerPosition=0;
-            System.out.println( "start position of player" +playerPosition );
+import java.util.Random;
+
+public class SnakeAndLadder {
+    public static int main(String[] args) {
+        int playerPosition = 0;
+        SnakeAndLadder Player1 = new SnakeAndLadder();
+        System.out.println(Player1);
+
+        public int rollDie () {
+            Random rand = new Random();
+            int diceRoll = rand.nextInt(6) + 1;
+            return diceRoll;
         }
+    }
 }
+
+
+
+
+
+
+
 
