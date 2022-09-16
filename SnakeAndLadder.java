6b9864a8264f7d7c466eacd52399960a6691c8cc
Ankit Ghosh <ankitghosh877@gmail.com>
2022-09-16 17:57:51 +0530

    rolling a dice

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,12 +3,14 @@ package com.codinclub;
 import java.util.Random;
 
 public class SnakeAndLadder {
-    public static int main(String[] args) {
+    public static int main(String[] args)
+    {
         int playerPosition = 0;
         SnakeAndLadder Player1 = new SnakeAndLadder();
         System.out.println(Player1);
 
-        public int rollDie() {
+        public int rollDie()
+    {
             Random rand = new Random();
             int diceRoll = rand.nextInt(6) + 1;
             return diceRoll;
