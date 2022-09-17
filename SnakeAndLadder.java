3c6137ab19c95bb765d21c9e6631c2fd4e37ba3a
Ankit Ghosh <ankitghosh877@gmail.com>
2022-09-17 10:37:48 +0530

    reach the winning position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,43 +1,52 @@
 package com.codinclub;
+
+import java.util.Random;
+
 public class SnakeAndLadder {
-    
-    public static final int NoPlay=1;
+
+    public static final int NoPlay = 1;
     public static final int LADDER = 2;
     public static final int SNAKE = 3;
 
     public static void main(String[] args) {
         int playerPosition = 0;
         System.out.println("position of player is : " + playerPosition);
-        int dieNumber = RandomDieValue();
-        System.out.println("you got : " + dieNumber);
-        int option = RandomOption();
-        System.out.println("The option is " + option);
 
-        switch (option) {
-            case LADDER:
-                playerPosition = playerPosition + dieNumber;
-                break;
-            case SNAKE:
-                playerPosition = playerPosition- dieNumber;
-                break;
-            default:
-                System.out.println("No play");
+        while (playerPosition < 100) {
+            int dieNumber = RandomDieValue();
+            System.out.println("you got : " + dieNumber);
+
+            int option = RandomOption();
+
+            switch (option) {
+                case LADDER:
+                    if(playerPosition>100)
+                        playerPosition=100;
+                    playerPosition = playerPosition + dieNumber;
+                    System.out.println( "Ladder :"+ LADDER );
+                    break;
+                case SNAKE:
+                    if(playerPosition<0)
+                        playerPosition=0;
+                    playerPosition = playerPosition - dieNumber;
+                    System.out.println( "Snake :" + SNAKE );
+                    break;
+                default:
+                    System.out.println("No play");
+            }
+            System.out.println( " current position of player :" +playerPosition );
         }
-        System.out.println(playerPosition);
+        System.out.println("congrats you win");
     }
-
-    private static int RandomOption() {
+    public static int RandomOption() {
         int RandomOption = (int) Math.floor((Math.random() * 3));
         return RandomOption;
     }
-
     private static int RandomDieValue() {
         int RandomDieValue = (int) Math.floor((Math.random() * 6) + 1);
         return RandomDieValue;
     }
 }
-
-
 
 
 
