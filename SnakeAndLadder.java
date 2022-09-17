14b2e6138dbfe129efac05e7deb22b701c8273d1
Ankit Ghosh <ankitghosh877@gmail.com>
2022-09-17 10:18:54 +0530

    check for a option

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,22 +1,42 @@
 package com.codinclub;
+public class SnakeAndLadder {
+    
+    public static final int NoPlay=1;
+    public static final int LADDER = 2;
+    public static final int SNAKE = 3;
 
-import java.util.Random;
-
-public class SnakeAndLadder
-{
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int playerPosition = 0;
-        int diceNumber;
         System.out.println("position of player is : " + playerPosition);
-        diceNumber = RandomDieNUmber();
-        System.out.println("you got : " + diceNumber);
+        int dieNumber = RandomDieValue();
+        System.out.println("you got : " + dieNumber);
+        int option = RandomOption();
+        System.out.println("The option is " + option);
+
+        switch (option) {
+            case LADDER:
+                playerPosition = playerPosition + dieNumber;
+                break;
+            case SNAKE:
+                playerPosition = playerPosition- dieNumber;
+                break;
+            default:
+                System.out.println("No play");
+        }
+        System.out.println(playerPosition);
     }
-    private static int RandomDieNUmber() {
-        int RandomValue = (int) Math.floor((Math.random() * 6) + 1);
-        return RandomValue;
+
+    private static int RandomOption() {
+        int RandomOption = (int) Math.floor((Math.random() * 3));
+        return RandomOption;
+    }
+
+    private static int RandomDieValue() {
+        int RandomDieValue = (int) Math.floor((Math.random() * 6) + 1);
+        return RandomDieValue;
     }
 }
+
 
 
 
